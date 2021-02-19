58977ef45ab55d4d3c39b3f29af0720a5e55e164
Ma-Rah <martin.lars.rahm@gmail.com>
2021-02-19 23:00:48 +0100

    first edit

--- js/index.js
+++ js/index.js
@@ -23,30 +23,30 @@ let direction = true;
 let rainbowColor = false;
 
 // functions section
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-	// true for mobile device
-	// wrapper.innerHTML = `<h1>Sorry, this site is currently not available on mobile devices.</h1>`;
-} else {
-	// false for not mobile device
-	function draw(e) {
-		if (!isDrawing) return; //only run when drawing
-		ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-		ctx.beginPath();
-		// Start from :
-		ctx.moveTo(lastX, lastY);
-		// Go to:
-		ctx.lineTo(e.offsetX, e.offsetY);
-		ctx.stroke();
-		[lastX, lastY] = [e.offsetX, e.offsetY];
+// if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+// true for mobile device
+// wrapper.innerHTML = `<h1>Sorry, this site is currently not available on mobile devices.</h1>`;
+// } else {
+// false for not mobile device
+function draw(e) {
+	if (!isDrawing) return; //only run when drawing
+	ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+	ctx.beginPath();
+	// Start from :
+	ctx.moveTo(lastX, lastY);
+	// Go to:
+	ctx.lineTo(e.offsetX, e.offsetY);
+	ctx.stroke();
+	[lastX, lastY] = [e.offsetX, e.offsetY];
 
-		if (rainbowColor === true) {
-			hue++;
-			if (hue >= 360) {
-				hue = 0;
-			}
+	if (rainbowColor === true) {
+		hue++;
+		if (hue >= 360) {
+			hue = 0;
 		}
 	}
 }
+// }
 
 const clear = () => ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -68,98 +68,11 @@ canvas.addEventListener("mousedown", (e) => {
 colorPicker.addEventListener("input", () => (colorPicker.style.color = `hsl(${hue}, 100%, 50%)`));
 clearBtn.addEventListener("click", clear);
 downloadBtn.addEventListener("click", download);
-canvas.addEventListener("mousemove", draw);
-canvas.addEventListener("mouseup", () => (isDrawing = false));
-canvas.addEventListener("mouseout", () => (isDrawing = false));
-
-// canvas.addEventListener("touchstart", draw);
-// canvas.addEventListener("touchmove", () => (isDrawing = false));
-// canvas.addEventListener("touchend", () => (isDrawing = false));
+canvas.addEventListener("pointermove", draw, true);
+canvas.addEventListener("pointerup", () => (isDrawing = false));
+canvas.addEventListener("pointerdown", () => (isDrawing = false));
 
 rainbow.addEventListener("click", () => {
 	rainbowColor = !rainbowColor ? true : false;
 	rainbow.classList.toggle("rainbow--toggled");
 });
-
-// Set up touch events for mobile, etc
-canvas.addEventListener(
-	"touchstart",
-	function (e) {
-		mousePos = getTouchPos(canvas, e);
-		var touch = e.touches[0];
-		var mouseEvent = new MouseEvent("mousedown", {
-			clientX: touch.clientX,
-			clientY: touch.clientY,
-		});
-		canvas.dispatchEvent(mouseEvent);
-	},
-	false
-);
-canvas.addEventListener(
-	"touchend",
-	function (e) {
-		var mouseEvent = new MouseEvent("mouseup", {});
-		canvas.dispatchEvent(mouseEvent);
-	},
-	false
-);
-canvas.addEventListener(
-	"touchmove",
-	function (e) {
-		var touch = e.touches[0];
-		var mouseEvent = new MouseEvent("mousemove", {
-			clientX: touch.clientX,
-			clientY: touch.clientY,
-		});
-		canvas.dispatchEvent(mouseEvent);
-	},
-	false
-);
-
-// Prevent scrolling when touching the canvas
-document.body.addEventListener(
-	"touchstart",
-	function (e) {
-		if (e.target == canvas) {
-			e.preventDefault();
-		}
-	},
-	false
-);
-document.body.addEventListener(
-	"touchend",
-	function (e) {
-		if (e.target == canvas) {
-			e.preventDefault();
-		}
-	},
-	false
-);
-document.body.addEventListener(
-	"touchmove",
-	function (e) {
-		if (e.target == canvas) {
-			e.preventDefault();
-		}
-	},
-	false
-);
-
-// Get the position of a touch relative to the canvas
-function getTouchPos(canvasDom, touchEvent) {
-	var rect = canvasDom.getBoundingClientRect();
-	return {
-		x: touchEvent.touches[0].clientX - rect.left,
-		y: touchEvent.touches[0].clientY - rect.top,
-	};
-}
-
-// Draw to the canvas
-function renderCanvas() {
-	if (drawing) {
-		ctx.moveTo(lastPos.x, lastPos.y);
-		ctx.lineTo(mousePos.x, mousePos.y);
-		ctx.stroke();
-		lastPos = mousePos;
-	}
-}
